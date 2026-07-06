007aee99ade770c9e5acdfcc185ef82ff0d629ad
Pedro Lucas <143295682+plpedreira@users.noreply.github.com>
2026-07-06 10:43:37 -0300

    feat: initialize project structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import PhilosophyCard from "./components/PhilosophyCard";
+const titulo = "Memento Mori";
+
+function App() {  
+  return (
+  <div>
+      <PhilosophyCard titulo={titulo} />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/PhilosophyCard.tsx
@@ -0,0 +1,20 @@
+function PhilosophyCard() {
+  const titulo = "memento mori";
+
+  return (
+    <div>
+      <h2>{titulo}</h2>
+
+      <p>
+        Lembre-se de que você vai morrer.
+      </p>
+
+      <p>
+        Memento Mori é uma expressão em latim utilizada para lembrar que a vida
+        é finita e que devemos aproveitar nosso tempo da melhor maneira possível.
+      </p>
+    </div>
+  );
+}
+
+export default PhilosophyCard;
