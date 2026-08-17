24ca4c7f49cdf55b04a437c8d24d1302fffa5111
Pedro Lucas <143295682+plpedreira@users.noreply.github.com>
2026-08-17 10:21:56 -0300

    feat: add philosophy favorites

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,25 @@ function App() {
   const hoje = new Date();
   const dia = hoje.getDate();
   const [indice, setIndice] = useState(dia % philosophies.length);
+  const [favoritos, setFavoritos] = useState <number[]>([]);
+
+  function proximaFilosofia() {
+    setIndice((indice + 1) % philosophies.length);
+  }
+
+      function favoritarFilosofia() {
+  setFavoritos([...favoritos, indice]);
+  }
 
   return (
     <div>
       <PhilosophyCard philosophy={philosophies[indice]} />
 
-      <button
-        onClick={() => {
-          setIndice((indice + 1)% philosophies.length);
-        }}
-      >
+      <button onClick={proximaFilosofia}>
         Outra filosofia
       </button>
+
+      <button onClick={favoritarFilosofia}>Favoritar</button>
     </div>
   );
 }
