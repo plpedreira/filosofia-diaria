0860597369d7a7ced3db07fa87460bf3f134e8b1
Pedro Lucas <143295682+plpedreira@users.noreply.github.com>
2026-08-18 10:12:27 -0300

    feat: add favorite functionality and button

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,22 @@ import { useState } from "react";
 function App() {
   const hoje = new Date();
   const dia = hoje.getDate();
+
   const [indice, setIndice] = useState(dia % philosophies.length);
-  const [favoritos, setFavoritos] = useState <number[]>([]);
+  const [favoritos, setFavoritos] = useState<number[]>([]);
 
   function proximaFilosofia() {
     setIndice((indice + 1) % philosophies.length);
   }
 
-      function favoritarFilosofia() {
-  setFavoritos([...favoritos, indice]);
+  function favoritarFilosofia() {
+    if (favoritos.includes(indice)) {
+      setFavoritos(
+        favoritos.filter((favorito) => favorito !== indice)
+      );
+    } else {
+      setFavoritos([...favoritos, indice]);
+    }
   }
 
   return (
@@ -24,7 +31,18 @@ function App() {
         Outra filosofia
       </button>
 
-      <button onClick={favoritarFilosofia}>Favoritar</button>
+      <button onClick={favoritarFilosofia}>
+        {favoritos.includes(indice) ? "Desfavoritar" : "Favoritar"}
+      </button>
+
+      <h2>Favoritos</h2>
+
+      {favoritos.map((favorito) => (
+        <PhilosophyCard
+          key={favorito}
+          philosophy={philosophies[favorito]}
+        />
+      ))}
     </div>
   );
 }
